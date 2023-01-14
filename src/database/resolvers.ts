898c403fa11517e2373dff800e42c80534584080
Kevin Andres <kevinandresgom19@gmail.com>
2023-01-13 20:24:10 -0500

    Add use jwt

--- src/database/resolvers.ts
+++ src/database/resolvers.ts
@@ -1,22 +1,47 @@
+//TODO: Typing createUser params & modularize.
 import UserModel from "../models/user.model";
+import bcrypt from 'bcryptjs';
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+const genToken = (user: any, secret: string, expiration: string) => {
+  const { id, email } = user;
+  return jwt.sign({ id, email }, secret, { expiresIn: expiration });
+}
+
 const resolvers = {
     Query: {
       
     },
     Mutation: {
       createUser: async (_parent: any, {input}: any) => {
-        const { email, /* password */ } = input;
+        const { email, password } = input;
         const exist = await UserModel.exists({email});
         if (exist) {
           throw new Error("The user already exist");
         }
         try {
+          const salt = await bcrypt.genSalt(10);
+          input.password = await bcrypt.hash(password, salt);
           const newUser = new UserModel(input);
           await newUser.save();
         } catch (e) {
-          console.log(e)
+          console.log(e);
         }
-        return "The user has successfully registered"
+        return "The user has successfully registered";
+      },
+      authUser: async (_parent: any, {input}: any) => {
+        const { email, password } = input;
+        const exist = await UserModel.findOne({email});
+        if (!exist) {
+          throw new Error("The user does'nt exits");
+        }
+        const isCorrectPassword: boolean = await bcrypt.compare(password, exist.password);
+        if(!isCorrectPassword) {
+          throw new Error("The password is incorrect")
+        }
+        const token: string = genToken(exist, <string> process.env.SECRET, "2h");
+        return { token };
       }
     }
 };
--- src/database/schema.ts
+++ src/database/schema.ts
@@ -8,14 +8,24 @@ const typeDefs = `
         books: [Book]
     }
 
+    type Token {
+        token: String
+    }
+
     input UserInput {
         name: String!
         email: String!
         password: String!
     }
 
+    input AuthUserInput {
+        email: String!
+        password: String!
+    }
+
     type Mutation {
         createUser(input: UserInput): String
+        authUser(input: AuthUserInput): Token
     }
 `;
 export default typeDefs;
